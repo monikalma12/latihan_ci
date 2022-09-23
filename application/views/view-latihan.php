b5a9fc7504a539ab8e940dfad01a838926f62c6f
monikalma12 <aristiasalmammonika12@gmail.com>
2022-09-23 18:06:34 +0700

    upload source code ci3

---
+++ application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk Kita Belajar Web Programming..!!! <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
